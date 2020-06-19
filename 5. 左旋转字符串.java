501119be7c5146bb69234b616d3edabc184d6e45
zifeiyu0531 <32443516+zifeiyu0531@users.noreply.github.com>
2020-06-19 19:52:54 +0800

    左旋字符串

---
+++ 5. 左旋转字符串.java
@@ -0,0 +1,31 @@
+// 字符串的左旋转操作是把字符串前面的若干个字符转移到字符串的尾部。请定义一个函数实现字符串左旋转操作的功能。比如，输入字符串"abcdefg"和数字2，该函数将返回左旋转两位得到的结果"cdefgab"。
+
+//  
+
+// 示例 1：
+
+// 输入: s = "abcdefg", k = 2
+// 输出: "cdefgab"
+// 示例 2：
+
+// 输入: s = "lrloseumgh", k = 6
+// 输出: "umghlrlose"
+//  
+
+// 限制：
+
+// 1 <= k < s.length <= 10000
+
+class Solution_5 {
+    public String reverseLeftWords(String s, int n) {
+        return s.substring(n) + s.substring(0, n);
+    }
+
+    public static void main(String args[]) {
+        Solution_5 solution = new Solution_5();
+        String s_1 = "abcdefg", s_2 = "lrloseumgh";
+        int k_1 = 2, k_2 = 6;
+        System.out.println(solution.reverseLeftWords(s_1, k_1));
+        System.out.println(solution.reverseLeftWords(s_2, k_2));
+    }
+}
