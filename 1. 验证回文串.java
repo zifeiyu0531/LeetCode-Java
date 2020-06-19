e4bd66d884a7509102c1e3ff3f1050401399a9a4
zifeiyu0531 <32443516+zifeiyu0531@users.noreply.github.com>
2020-06-19 16:29:29 +0800

    验证回文串

---
+++ 1. 验证回文串.java
@@ -0,0 +1,38 @@
+// 给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
+
+// 说明：本题中，我们将空字符串定义为有效的回文串。
+
+// 示例 1:
+
+// 输入: "A man, a plan, a canal: Panama"
+// 输出: true
+// 示例 2:
+
+// 输入: "race a car"
+// 输出: false
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+        while (i < j) {
+            while(i < j && !Character.isLetterOrDigit(s.charAt(i))){
+                i++;
+            }
+            while(i < j && !Character.isLetterOrDigit(s.charAt(j))){
+                j--;
+            }
+            if(i < j && Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    public static void main(String args[]) {
+        Solution solution = new Solution();
+        System.out.println(solution.isPalindrome("A man, a plan, a canal: Panama"));
+        System.out.println(solution.isPalindrome("race a car"));
+    }
+}
