159ee8506f97e9e57b167c2c33b14574e4785ace
zifeiyu0531 <32443516+zifeiyu0531@users.noreply.github.com>
2020-06-22 18:13:06 +0800

    无重复字符的最长子串

---
+++ 33. 无重复字符的最长子串.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+// 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+
+// 示例 1:
+
+// 输入: "abcabcbb"
+// 输出: 3 
+// 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+// 示例 2:
+
+// 输入: "bbbbb"
+// 输出: 1
+// 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+// 示例 3:
+
+// 输入: "pwwkew"
+// 输出: 3
+// 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+//      请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+
+class Solution_33 {
+    public int lengthOfLongestSubstring(String s) {
+        Set<Character> set = new HashSet<Character>();
+        int len = 0;
+        int j = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (i != 0) {
+                set.remove(s.charAt(i - 1));
+            }
+            while (j < s.length() && !set.contains(s.charAt(j))) {
+                set.add(s.charAt(j));
+                j++;
+            }
+            len = Math.max(j - i, len);
+        }
+        return len;
+    }
+
+    public static void main(String args[]) {
+        Solution_33 solution = new Solution_33();
+        String s_1 = "abcabcbb", s_2 = "bbbbb", s_3 = "pwwkew";
+        System.out.println(solution.lengthOfLongestSubstring(s_1));
+        System.out.println(solution.lengthOfLongestSubstring(s_2));
+        System.out.println(solution.lengthOfLongestSubstring(s_3));
+    }
+}
