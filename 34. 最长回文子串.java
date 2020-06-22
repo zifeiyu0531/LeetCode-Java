713175398f423f01c149f861584fb19463561231
zifeiyu0531 <32443516+zifeiyu0531@users.noreply.github.com>
2020-06-22 19:36:12 +0800

    最长回文子串

---
+++ 34. 最长回文子串.java
@@ -0,0 +1,46 @@
+// 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+
+// 示例 1：
+
+// 输入: "babad"
+// 输出: "bab"
+// 注意: "aba" 也是一个有效答案。
+// 示例 2：
+
+// 输入: "cbbd"
+// 输出: "bb"
+
+class Solution_34 {
+    public String longestPalindrome(String s) {
+        if (s.length() == 0 || s == null) {
+            return s;
+        }
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    public int expandAroundCenter(String s, int left, int right) {
+        int l = left, r = right;
+        while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+            l--;
+            r++;
+        }
+        return r - l - 1;
+    }
+
+    public static void main(String args[]) {
+        Solution_34 solution = new Solution_34();
+        String s_1 = "babad", s_2 = "cbbd";
+        System.out.println(solution.longestPalindrome(s_1));
+        System.out.println(solution.longestPalindrome(s_2));
+    }
+}
