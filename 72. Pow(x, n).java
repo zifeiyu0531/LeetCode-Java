8f77bff21da88374b2ce3508dfb1abf4c5bf5d49
zifeiyu0531 <32443516+zifeiyu0531@users.noreply.github.com>
2020-06-26 11:47:20 +0800

    Pow(x, n)

---
+++ 72. Pow(x, n).java
@@ -0,0 +1,53 @@
+// 实现 pow(x, n) ，即计算 x 的 n 次幂函数。
+
+// 示例 1:
+
+// 输入: 2.00000, 10
+// 输出: 1024.00000
+// 示例 2:
+
+// 输入: 2.10000, 3
+// 输出: 9.26100
+// 示例 3:
+
+// 输入: 2.00000, -2
+// 输出: 0.25000
+// 解释: 2-2 = 1/22 = 1/4 = 0.25
+// 说明:
+
+// -100.0 < x < 100.0
+// n 是 32 位有符号整数，其数值范围是 [−231, 231 − 1] 。
+
+class Solution_72 {
+    public double myPow(double x, int n) {
+        long N = n;
+        return N >= 0 ? quickMul(x, N) : 1.0 / quickMul(x, -N);
+    }
+
+    double quickMul(double x, long N) {
+        double ans = 1.0;
+        // 贡献的初始值为 x
+        double x_contribute = x;
+        // 在对 N 进行二进制拆分的同时计算答案
+        while (N > 0) {
+            if (N % 2 == 1) {
+                // 如果 N 二进制表示的最低位为 1，那么需要计入贡献
+                ans *= x_contribute;
+            }
+            // 将贡献不断地平方
+            x_contribute *= x_contribute;
+            // 舍弃 N 二进制表示的最低位，这样我们每次只要判断最低位即可
+            N /= 2;
+        }
+        return ans;
+    }
+
+    public static void main(String args[]) {
+        Solution_72 solution = new Solution_72();
+        double x_1 = 2.00000, x_2 = 2.10000, x_3 = 2.00000;
+        int n_1 = 10, n_2 = 3, n_3 = -2;
+        System.out.println(solution.myPow(x_1, n_1));
+        System.out.println(solution.myPow(x_2, n_2));
+        System.out.println(solution.myPow(x_3, n_3));
+    }
+}
