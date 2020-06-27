9920351ccad0a6385ce5d30bea05ea1f5115ec21
zifeiyu0531 <32443516+zifeiyu0531@users.noreply.github.com>
2020-06-27 14:44:17 +0800

    爬楼梯

---
+++ 86. 爬楼梯.java
@@ -0,0 +1,43 @@
+// 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+
+// 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+
+// 注意：给定 n 是一个正整数。
+
+// 示例 1：
+
+// 输入： 2
+// 输出： 2
+// 解释： 有两种方法可以爬到楼顶。
+// 1.  1 阶 + 1 阶
+// 2.  2 阶
+// 示例 2：
+
+// 输入： 3
+// 输出： 3
+// 解释： 有三种方法可以爬到楼顶。
+// 1.  1 阶 + 1 阶 + 1 阶
+// 2.  1 阶 + 2 阶
+// 3.  2 阶 + 1 阶
+
+class Solution_86 {
+    public int climbStairs(int n) {
+        int[] steps = new int[n];
+        if (n <= 2) {
+            return n;
+        }
+        steps[0] = 1;
+        steps[1] = 2;
+        for (int i = 2; i < n; i++) {
+            steps[i] = steps[i - 1] + steps[i - 2];
+        }
+        return steps[n - 1];
+    }
+
+    public static void main(String args[]) {
+        Solution_86 solution = new Solution_86();
+        int n_1 = 2, n_2 = 3;
+        System.out.println(solution.climbStairs(n_1));
+        System.out.println(solution.climbStairs(n_2));
+    }
+}
