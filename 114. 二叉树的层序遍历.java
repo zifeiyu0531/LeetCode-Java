14ae17e3586bb91e4513f959672e4cf05705b976
zifeiyu0531 <32443516+zifeiyu0531@users.noreply.github.com>
2020-06-30 12:43:48 +0800

    二叉树的层序遍历

---
+++ 114. 二叉树的层序遍历.java
@@ -0,0 +1,49 @@
+import java.util.*;
+// 给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
+
+//  
+
+// 示例：
+// 二叉树：[3,9,20,null,null,15,7],
+
+//     3
+//    / \
+//   9  20
+//     /  \
+//    15   7
+// 返回其层次遍历结果：
+
+// [
+//   [3],
+//   [9,20],
+//   [15,7]
+// ]
+
+class Solution_114 {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        if (root != null) {
+            queue.add(root);
+        }
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> nums = new ArrayList<Integer>();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode node = queue.poll();
+                nums.add(node.val);
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            ans.add(nums);
+        }
+        return ans;
+    }
+
+    public static void main(String args[]) {
+    }
+}
