89fec2ad528a1365a6565f8ee6a0ddbcb7af6afc
zifeiyu0531 <1186352914@qq.com>
2020-07-01 14:12:31 +0800

    平衡二叉树

---
+++ 123. 平衡二叉树.java
@@ -0,0 +1,46 @@
+// 给定一个二叉树，判断它是否是高度平衡的二叉树。
+
+// 本题中，一棵高度平衡二叉树定义为：
+
+// 一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过1。
+
+// 示例 1:
+
+// 给定二叉树 [3,9,20,null,null,15,7]
+
+//     3
+//    / \
+//   9  20
+//     /  \
+//    15   7
+// 返回 true 。
+
+// 示例 2:
+
+// 给定二叉树 [1,2,2,3,3,null,null,4,4]
+
+//        1
+//       / \
+//      2   2
+//     / \
+//    3   3
+//   / \
+//  4   4
+// 返回 false 。
+
+class Solution_123 {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return Math.abs(getDepth(root.left) - getDepth(root.right)) <= 1 && isBalanced(root.left)
+                && isBalanced(root.right);
+    }
+
+    public int getDepth(TreeNode root) {
+        return root == null ? 0 : Math.max(getDepth(root.left) + 1, getDepth(root.right) + 1);
+    }
+
+    public static void main(String args[]) {
+    }
+}
