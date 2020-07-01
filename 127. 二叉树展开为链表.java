e9e75d907731223cfb65b32d80ccf33be2294610
zifeiyu0531 <32443516+zifeiyu0531@users.noreply.github.com>
2020-07-01 19:44:42 +0800

    二叉树展开为链表

---
+++ 127. 二叉树展开为链表.java
@@ -0,0 +1,46 @@
+// 给定一个二叉树，原地将它展开为一个单链表。
+
+//  
+
+// 例如，给定二叉树
+
+//     1
+//    / \
+//   2   5
+//  / \   \
+// 3   4   6
+// 将其展开为：
+
+// 1
+//  \
+//   2
+//    \
+//     3
+//      \
+//       4
+//        \
+//         5
+//          \
+//           6
+
+class Solution_127 {
+    public void flatten(TreeNode root) {
+        while (root != null) {
+            if (root.left == null) {
+                root = root.right;
+            } else {
+                TreeNode pre = root.left;
+                while (pre.right != null) {
+                    pre = pre.right;
+                }
+                pre.right = root.right;
+                root.right = root.left;
+                root.left = null;
+                root = root.right;
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+    }
+}
