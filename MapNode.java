c8356d3b6d4657f368d672a2c41bc05de3dee683
zifeiyu0531 <32443516+zifeiyu0531@users.noreply.github.com>
2020-07-03 17:34:39 +0800

    图节点MapNode

--- MapNode.java
+++ MapNode.java
@@ -2,19 +2,19 @@ import java.util.*;
 
 public class MapNode {
     public int val;
-    public List<Node> neighbors;
+    public List<MapNode> neighbors;
 
     public MapNode() {
         val = 0;
-        neighbors = new ArrayList<Node>();
+        neighbors = new ArrayList<MapNode>();
     }
 
     public MapNode(int val) {
         this.val = val;
-        neighbors = new ArrayList<Node>();
+        neighbors = new ArrayList<MapNode>();
     }
 
-    public MapNode(int val, ArrayList<Node> neighbors) {
+    public MapNode(int val, ArrayList<MapNode> neighbors) {
         this.val = val;
         this.neighbors = neighbors;
     }
