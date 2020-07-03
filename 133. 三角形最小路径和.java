f654669ec8b48e4b72e44c3d699bbf461a0e7e87
zifeiyu0531 <32443516+zifeiyu0531@users.noreply.github.com>
2020-07-03 14:32:27 +0800

    三角形最小路径和

---
+++ 133. 三角形最小路径和.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+// 给定一个三角形，找出自顶向下的最小路径和。每一步只能移动到下一行中相邻的结点上。
+
+// 相邻的结点 在这里指的是 下标 与 上一层结点下标 相同或者等于 上一层结点下标 + 1 的两个结点。
+
+//  
+
+// 例如，给定三角形：
+
+// [
+//      [2],
+//     [3,4],
+//    [6,5,7],
+//   [4,1,8,3]
+// ]
+// 自顶向下的最小路径和为 11（即，2 + 3 + 5 + 1 = 11）。
+
+//  
+
+// 说明：
+
+// 如果你可以只使用 O(n) 的额外空间（n 为三角形的总行数）来解决这个问题，那么你的算法会很加分。
+
+class Solution_133 {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        if (triangle == null || triangle.size() == 0) {
+            return 0;
+        }
+        int[] dp = new int[triangle.size() + 1];
+        for (int i = triangle.size() - 1; i >= 0; i--) {
+            List<Integer> nums = triangle.get(i);
+            for (int j = 0; j < nums.size(); j++) {
+                dp[j] = Math.min(dp[j], dp[j + 1]) + nums.get(j);
+            }
+        }
+        return dp[0];
+    }
+
+    public static void main(String args[]) {
+        Solution_133 solution = new Solution_133();
+        List<List<Integer>> triangle = new ArrayList<List<Integer>>(Arrays.asList(Arrays.asList(2), Arrays.asList(3, 4),
+                Arrays.asList(6, 5, 7), Arrays.asList(4, 1, 8, 3)));
+        System.out.println(solution.minimumTotal(triangle));
+    }
+}
