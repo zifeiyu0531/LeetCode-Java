cc90971d7069ff0521368e258cb295ceed8a5e95
zifeiyu0531 <32443516+zifeiyu0531@users.noreply.github.com>
2020-07-07 13:51:53 +0800

    带随机指针的链表节点

---
+++ RandomNode.java
@@ -0,0 +1,11 @@
+public class RandomNode {
+    int val;
+    Node next;
+    Node random;
+
+    public RandomNode(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
