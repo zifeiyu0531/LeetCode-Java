9c0229d89679a1008bc896356b7d7dcff207cc85
zifeiyu0531 <32443516+zifeiyu0531@users.noreply.github.com>
2020-07-07 14:19:34 +0800

    环形链表

--- 145. 环形链表.java
+++ 145. 环形链表.java
@@ -30,9 +30,12 @@
 
 class Solution_145 {
     public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
         ListNode p1 = head;
         ListNode p2 = head;
-        while (p1 != null && p2.next != null) {
+        while (p1.next != null && p2.next.next != null) {
             p1 = p1.next;
             p2 = p2.next.next;
             if (p1 == p2) {
