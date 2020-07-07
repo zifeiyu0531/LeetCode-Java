32e0dc65d011fe207e7b5655cbacaf4718b5f33e
zifeiyu0531 <32443516+zifeiyu0531@users.noreply.github.com>
2020-07-07 14:16:58 +0800

    环形链表

---
+++ 145. 环形链表.java
@@ -0,0 +1,47 @@
+// 给定一个链表，判断链表中是否有环。
+
+// 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
+
+//  
+
+// 示例 1：
+
+// 输入：head = [3,2,0,-4], pos = 1
+// 输出：true
+// 解释：链表中有一个环，其尾部连接到第二个节点。
+
+// 示例 2：
+
+// 输入：head = [1,2], pos = 0
+// 输出：true
+// 解释：链表中有一个环，其尾部连接到第一个节点。
+
+// 示例 3：
+
+// 输入：head = [1], pos = -1
+// 输出：false
+// 解释：链表中没有环。
+
+//  
+
+// 进阶：
+
+// 你能用 O(1)（即，常量）内存解决此问题吗？
+
+class Solution_145 {
+    public boolean hasCycle(ListNode head) {
+        ListNode p1 = head;
+        ListNode p2 = head;
+        while (p1 != null && p2 != null) {
+            p1 = p1.next;
+            p2 = p2.next.next;
+            if (p1 == p2) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String args[]) {
+    }
+}
