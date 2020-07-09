0baa288bbc108a5a5efbe229d2bd34ab198b3473
zifeiyu0531 <32443516+zifeiyu0531@users.noreply.github.com>
2020-07-09 18:53:26 +0800

    链表排序

--- 数据结构与算法/1 链表/3 链表排序.java
+++ 数据结构与算法/1 链表/3 链表排序.java
@@ -1,4 +1,10 @@
 class Solution_1 {
+    /**
+     * 链表排序
+     * 
+     * @param head 待排序链表头节点
+     * @return 排序后链表头节点
+     */
     public LinkedNode<Integer> sort(LinkedNode<Integer> head) {
         if (head == null) {
             return null;
