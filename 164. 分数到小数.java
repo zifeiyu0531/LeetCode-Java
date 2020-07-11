81936e173c488009f507b450139c9f4c3d3c23ec
zifeiyu0531 <32443516+zifeiyu0531@users.noreply.github.com>
2020-07-11 12:09:32 +0800

    分数到小数

---
+++ 164. 分数到小数.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+// 给定两个整数，分别表示分数的分子 numerator 和分母 denominator，以字符串形式返回小数。
+
+// 如果小数部分为循环小数，则将循环的部分括在括号内。
+
+// 示例 1:
+
+// 输入: numerator = 1, denominator = 2
+// 输出: "0.5"
+// 示例 2:
+
+// 输入: numerator = 2, denominator = 1
+// 输出: "2"
+// 示例 3:
+
+// 输入: numerator = 2, denominator = 3
+// 输出: "0.(6)"
+
+class Solution_164 {
+    public String fractionToDecimal(int numerator, int denominator) {
+        if (numerator == 0) {
+            return "0";
+        }
+        StringBuilder fraction = new StringBuilder();
+        // If either one is negative (not both)
+        if (numerator < 0 ^ denominator < 0) {
+            fraction.append("-");
+        }
+        // Convert to Long or else abs(-2147483648) overflows
+        long dividend = Math.abs(Long.valueOf(numerator));
+        long divisor = Math.abs(Long.valueOf(denominator));
+        fraction.append(String.valueOf(dividend / divisor));
+        long remainder = dividend % divisor;
+        if (remainder == 0) {
+            return fraction.toString();
+        }
+        fraction.append(".");
+        Map<Long, Integer> map = new HashMap<>();
+        while (remainder != 0) {
+            if (map.containsKey(remainder)) {
+                fraction.insert(map.get(remainder), "(");
+                fraction.append(")");
+                break;
+            }
+            map.put(remainder, fraction.length());
+            remainder *= 10;
+            fraction.append(String.valueOf(remainder / divisor));
+            remainder %= divisor;
+        }
+        return fraction.toString();
+    }
+
+    public static void main(String args[]) {
+        Solution_164 solution = new Solution_164();
+        int numerator_1 = 1, numerator_2 = 2, numerator_3 = 2;
+        int denominator_1 = 2, denominator_2 = 1, denominator_3 = 3;
+        System.out.println(solution.fractionToDecimal(numerator_1, denominator_1));
+        System.out.println(solution.fractionToDecimal(numerator_2, denominator_2));
+        System.out.println(solution.fractionToDecimal(numerator_3, denominator_3));
+    }
+}
