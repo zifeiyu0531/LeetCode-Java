6ca1bc047f4d8f2ee20d9acde6bc8967106976aa
zifeiyu0531 <32443516+zifeiyu0531@users.noreply.github.com>
2020-07-25 18:26:50 +0800

    冒泡排序

--- 数据结构与算法/6 排序算法/3 冒泡排序.java
+++ 数据结构与算法/6 排序算法/3 冒泡排序.java
@@ -1,6 +1,9 @@
-import java.util.*;
-
-class Solution {
+class Solution_3 {
+    /**
+     * 冒泡排序
+     * 
+     * @param nums 待排序数组
+     */
     void bubbleSort(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = nums.length - 1; j > i; j--) {
@@ -11,12 +14,5 @@ class Solution {
                 }
             }
         }
-    }
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        int[] nums = { 7, 3, 6, 5, 2, 0, 8 };
-        solution.bubbleSort(nums);
-        System.out.println(Arrays.toString(nums));
     }
 }
