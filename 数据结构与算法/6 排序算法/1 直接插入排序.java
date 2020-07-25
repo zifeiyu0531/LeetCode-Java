a481969326d9f01a6dd1c90e29e4855c7fdf9bb1
zifeiyu0531 <32443516+zifeiyu0531@users.noreply.github.com>
2020-07-25 17:58:52 +0800

    直接插入排序

--- 数据结构与算法/6 排序算法/1 直接插入排序.java
+++ 数据结构与算法/6 排序算法/1 直接插入排序.java
@@ -1,6 +1,8 @@
-import java.util.Arrays;
-
 class Solution {
+    /**
+     * 直接插入排序
+     * @param nums 待排序数组
+     */
     void insertSort(int[] nums) {
         for (int i = 1; i < nums.length; i++) {
             if (nums[i] < nums[i - 1]) {
@@ -13,12 +15,5 @@ class Solution {
                 nums[j + 1] = temp;
             }
         }
-    }
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        int[] nums = { 7, 3, 6, 5, 2, 0, 8 };
-        solution.insertSort(nums);
-        System.out.println(Arrays.toString(nums));
     }
 }
