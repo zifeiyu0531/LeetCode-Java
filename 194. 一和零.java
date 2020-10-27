89a92e16646788e8c3286a54a63e09655b194246
zifeiyu0531 <1186352914@qq.com>
2020-10-27 13:28:34 +0800

    一和零

---
+++ 194. 一和零.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+// 给你一个二进制字符串数组 strs 和两个整数 m 和 n 。
+
+// 请你找出并返回 strs 的最大子集的大小，该子集中 最多 有 m 个 0 和 n 个 1 。
+
+// 如果 x 的所有元素也是 y 的元素，集合 x 是集合 y 的 子集 。
+
+//  
+
+// 示例 1：
+
+// 输入：strs = ["10", "0001", "111001", "1", "0"], m = 5, n = 3
+// 输出：4
+// 解释：最多有 5 个 0 和 3 个 1 的最大子集是 {"10","0001","1","0"} ，因此答案是 4 。
+// 其他满足题意但较小的子集包括 {"0001","1"} 和 {"10","1","0"} 。{"111001"} 不满足题意，因为它含 4 个 1 ，大于 n 的值 3 。
+// 示例 2：
+
+// 输入：strs = ["10", "0", "1"], m = 1, n = 1
+// 输出：2
+// 解释：最大的子集是 {"0", "1"} ，所以答案是 2 。
+//  
+
+// 提示：
+
+// 1 <= strs.length <= 600
+// 1 <= strs[i].length <= 100
+// strs[i] 仅由 '0' 和 '1' 组成
+// 1 <= m, n <= 100
+
+class Solution_194 {
+    public int findMaxForm(String[] strs, int m, int n) {
+        int[][] dp = new int[m + 1][n + 1];
+        for (String s: strs) {
+            int[] count = countzeroesones(s);
+            for (int zeroes = m; zeroes >= count[0]; zeroes--)
+                for (int ones = n; ones >= count[1]; ones--)
+                    dp[zeroes][ones] = Math.max(1 + dp[zeroes - count[0]][ones - count[1]], dp[zeroes][ones]);
+        }
+        return dp[m][n];
+    }
+    
+    public int[] countzeroesones(String s) {
+        int[] c = new int[2];
+        for (int i = 0; i < s.length(); i++) {
+            c[s.charAt(i)-'0']++;
+        }
+        return c;
+    }
+
+    public static void main(String args[]) {
+        Solution_194 solution = new Solution_194();
+        String[] strs_1 = { "10", "0001", "111001", "1", "0" }, strs_2 = { "10", "0", "1" };
+        int m_1 = 5, m_2 = 1;
+        int n_1 = 3, n_2 = 1;
+        System.out.println(solution.findMaxForm(strs_1, m_1, n_1));
+        System.out.println(solution.findMaxForm(strs_2, m_2, n_2));
+    }
+}
