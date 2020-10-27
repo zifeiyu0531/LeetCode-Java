ef5eb8f7ffcd2984d2ec27ef10e7795499e6d56f
zifeiyu0531 <1186352914@qq.com>
2020-10-27 14:00:09 +0800

    最长上升子序列

---
+++ 195. 最长上升子序列.java
@@ -0,0 +1,40 @@
+// 给定一个无序的整数数组，找到其中最长上升子序列的长度。
+
+// 示例:
+
+// 输入: [10,9,2,5,3,7,101,18]
+// 输出: 4 
+// 解释: 最长的上升子序列是 [2,3,7,101]，它的长度是 4。
+// 说明:
+
+// 可能会有多种最长上升子序列的组合，你只需要输出对应的长度即可。
+// 你算法的时间复杂度应该为 O(n2) 。
+// 进阶: 你能将算法的时间复杂度降低到 O(n log n) 吗?
+
+class Solution_195 {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        if (n <= 0) {
+            return 0;
+        }
+        int[] dp = new int[n];
+        dp[0] = 1;
+        int max = 1;
+        for (int i = 1; i < n; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            max = Math.max(dp[i], max);
+        }
+        return max;
+    }
+
+    public static void main(String args[]) {
+        Solution_195 solution = new Solution_195();
+        int[] nums = { 10, 9, 2, 5, 3, 7, 101, 18 };
+        System.out.println(solution.lengthOfLIS(nums));
+    }
+}
