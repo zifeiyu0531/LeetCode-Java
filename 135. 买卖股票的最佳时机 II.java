8505633699a445954eea5c3a92c26ed35a7d54ad
zifeiyu0531 <32443516+zifeiyu0531@users.noreply.github.com>
2020-11-11 21:36:11 +0800

    买卖股票的最佳时机 II

--- 135. 买卖股票的最佳时机 II.java
+++ 135. 买卖股票的最佳时机 II.java
@@ -34,15 +34,12 @@
 class Solution_135 {
     public int maxProfit(int[] prices) {
         int profit = 0;
-        int i = 0, j = 1;
-        while (j < prices.length) {
-            if (prices[i] >= prices[j]) {
-                i = j;
-            } else {
+        int i = 0;
+        for (int j = 1; j < prices.length; j++) {
+            if(prices[j] > prices[i]){
                 profit += prices[j] - prices[i];
-                i++;
             }
-            j++;
+            i = j;
         }
         return profit;
     }
