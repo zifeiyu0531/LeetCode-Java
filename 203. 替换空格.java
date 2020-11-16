f988e8d96b238190ec72bc0bad5d3bd860f19c74
zifeiyu0531 <32443516+zifeiyu0531@users.noreply.github.com>
2020-11-16 18:39:52 +0800

    替换空格

---
+++ 203. 替换空格.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+// 请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
+
+//  
+
+// 示例 1：
+
+// 输入：s = "We are happy."
+// 输出："We%20are%20happy."
+//  
+
+// 限制：
+
+// 0 <= s 的长度 <= 10000
+
+class Solution_203 {
+    public String replaceSpace(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' ') {
+                sb.append("%20");
+            } else {
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String args[]) {
+        Solution_203 solution = new Solution_203();
+        String s_1 = "We are happy.";
+        System.out.println(solution.replaceSpace(s_1));
+    }
+}
