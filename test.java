41cf3115e7c1ac219038bfdda8e4f053561ccafb
zifeiyu0531 <32443516+zifeiyu0531@users.noreply.github.com>
2020-11-21 14:13:30 +0800

    Update test.java

--- test.java
+++ test.java
@@ -2,35 +2,19 @@ import java.util.*;
 
 class Solution {
 
-    private int partition(int[] nums, int left, int right) {
-        int temp = nums[left];
-        while (left < right) {
-            while (left < right && nums[right] >= temp) {
-                right--;
+    public void sort(Character[] array){
+        Arrays.sort(array, new Comparator<Character>(){
+            @Override
+            public int compare(Character o1, Character o2){
+                return o1 - o2;
             }
-            nums[left] = nums[right];
-            while (left < right && nums[left] <= temp) {
-                left++;
-            }
-            nums[right] = nums[left];
-        }
-        nums[left] = temp;
-        return left;
-    }
-
-    private void quickSort(int[] nums, int left, int right) {
-        if (left < right) {
-            int index = partition(nums, left, right);
-            quickSort(nums, left, index - 1);
-            quickSort(nums, index + 1, right);
-        }
+        });
     }
 
     public static void main(String args[]) {
         Solution solution = new Solution();
-        int[] nums = { 3, 1 };
-        solution.quickSort(nums, 0, nums.length - 1);
-        System.out.println(Arrays.toString(nums));
-        // System.out.println(solution.permuteUnique(num).toString());
+        Character[] array = {'c', 'd', 'a', 'f', 'b', 'e'};
+        solution.sort(array);
+        System.out.println(Arrays.toString(array));
     }
 }
