d117313ab6a292565e320f374c3a045ba16bfa49
zifeiyu0531 <32443516+zifeiyu0531@users.noreply.github.com>
2020-11-21 15:08:12 +0800

    单例模式-饿汉式

---
+++ 设计模式/1 单例模式/SingletonHungry.java
@@ -0,0 +1,10 @@
+public class SingletonHungry {
+    private static SingletonHungry instance = new SingletonHungry();
+
+    private SingletonHungry() {
+    }
+
+    public static SingletonHungry getInstance() {
+        return instance;
+    }
+}
