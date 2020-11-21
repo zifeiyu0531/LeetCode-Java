ee70050f3cb2cea59e37181a83e69f1e48907b5c
zifeiyu0531 <32443516+zifeiyu0531@users.noreply.github.com>
2020-11-21 20:36:47 +0800

    调整数组顺序使奇数位于偶数前面

---
+++ 215. 调整数组顺序使奇数位于偶数前面.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+// 输入一个整数数组，实现一个函数来调整该数组中数字的顺序，使得所有奇数位于数组的前半部分，所有偶数位于数组的后半部分。
+
+//  
+
+// 示例：
+
+// 输入：nums = [1,2,3,4]
+// 输出：[1,3,2,4] 
+// 注：[3,1,2,4] 也是正确的答案之一。
+//  
+
+// 提示：
+
+// 1 <= nums.length <= 50000
+// 1 <= nums[i] <= 10000
+
+class Solution_215 {
+    public int[] exchange(int[] nums) {
+        int i = 0, j = nums.length - 1;
+        while (i < j) {
+            while (i < j && (nums[i] & 1) == 1) {
+                i++;
+            }
+            while (i < j && (nums[j] & 1) == 0) {
+                j--;
+            }
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+        return nums;
+    }
+
+    public static void main(String args[]) {
+        Solution_215 solution = new Solution_215();
+        int[] nums = { 1, 2, 3, 4 };
+        System.out.println(Arrays.toString(solution.exchange(nums)));
+    }
+}
