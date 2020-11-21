88601996da33aefbb5d86f441882c25d3893eddf
zifeiyu0531 <32443516+zifeiyu0531@users.noreply.github.com>
2020-11-21 15:06:24 +0800

    单例模式-懒汉式

---
+++ 设计模式/1 单例模式/SingletonLazy.java
@@ -0,0 +1,13 @@
+public class SingletonLazy {
+    private static SingletonLazy instance;
+
+    private SingletonLazy() {
+    }
+
+    public static synchronized SingletonLazy getInstance() {
+        if (instance == null) {
+            instance = new SingletonLazy();
+        }
+        return instance;
+    }
+}
