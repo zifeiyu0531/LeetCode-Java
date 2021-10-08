b4ce364779a504d0f02a50b0d3e2c0f0bbb0ca2c
zifeiyu0531 <32443516+zifeiyu0531@users.noreply.github.com>
2021-10-08 13:09:29 +0800

    检查平衡性

---
+++ 289. 检查平衡性.java
@@ -0,0 +1,35 @@
+// 实现一个函数，检查二叉树是否平衡。在这个问题中，平衡树的定义如下：任意一个节点，其两棵子树的高度差不超过 1。
+
+// 示例 1:
+// 给定二叉树 [3,9,20,null,null,15,7]
+//     3
+//    / \
+//   9  20
+//     /  \
+//    15   7
+// 返回 true 。
+// 示例 2:
+// 给定二叉树 [1,2,2,3,3,null,null,4,4]
+//       1
+//      / \
+//     2   2
+//    / \
+//   3   3
+//  / \
+// 4   4
+// 返回 false 。
+class Solution_289 {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return Math.abs(depth(root.left) - depth(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    private int depth(TreeNode root) {
+        return root == null ? 0 : 1 + Math.max(depth(root.left), depth(root.right));
+    }
+
+    public static void main(String args[]) {
+    }
+}
