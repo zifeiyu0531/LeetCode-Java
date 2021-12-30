36c54b8564ff5442c88ffb3d0e76107831d962ce
zifeiyu0531 <32443516+zifeiyu0531@users.noreply.github.com>
2021-12-30 22:04:52 +0800

    T9键盘

--- 326. T9键盘.java
+++ 326. T9键盘.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 // 在老式手机上，用户通过数字键盘输入，手机将提供与这些数字相匹配的单词列表。每个数字映射到0至4个字母。给定一个数字序列，实现一个算法来返回匹配单词的列表。你会得到一张含有有效单词的列表。映射如下图所示：
 
