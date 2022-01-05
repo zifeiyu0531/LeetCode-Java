30cc7f240e4f6bf11cfef62d36a25d16b8415d9f
zifeiyu0531 <32443516+zifeiyu0531@users.noreply.github.com>
2022-01-05 14:57:18 +0800

    翻转二叉树

---
+++ 339. 翻转二叉树.java
@@ -0,0 +1,57 @@
+// 翻转一棵二叉树。
+
+// 示例：
+
+// 输入：
+
+//      4
+//    /   \
+//   2     7
+//  / \   / \
+// 1   3 6   9
+// 输出：
+
+//      4
+//    /   \
+//   7     2
+//  / \   / \
+// 9   6 3   1
+// 备注:
+// 这个问题是受到 Max Howell 的 原问题 启发的 ：
+
+// 谷歌：我们90％的工程师使用您编写的软件(Homebrew)，但是您却无法在面试时在白板上写出翻转二叉树这道题，这太糟糕了。
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution_339 {
+    public TreeNode invertTree(TreeNode root) {
+        invertChildren(root);
+        return root;
+    }
+
+    private void invertChildren(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        invertChildren(root.left);
+        invertChildren(root.right);
+    }
+
+    public static void main(String args[]) {
+    }
+}
