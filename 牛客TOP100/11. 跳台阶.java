8955b1a5a2804052e4fc3170b35ae8fece4292f9
zifeiyu0531 <32443516+zifeiyu0531@users.noreply.github.com>
2022-01-18 15:05:04 +0800

    跳台阶

---
+++ 牛客TOP100/11. 跳台阶.java
@@ -0,0 +1,13 @@
+package 牛客TOP100;
+
+class Solution {
+    public int jumpFloor(int target) {
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= target; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[target];
+    }
+}
