a0f96edd0efcf94015e9f9312b100d2f0cd48227
zifeiyu0531 <32443516+zifeiyu0531@users.noreply.github.com>
2022-01-18 15:48:19 +0800

    判断链表中是否有环

---
+++ 牛客TOP100/15. 判断链表中是否有环.java
@@ -0,0 +1,29 @@
+package 牛客TOP100;
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) {
+ * val = x;
+ * next = null;
+ * }
+ * }
+ */
+class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+        ListNode slow = head, fast = head.next;
+        while (fast != null && fast.next != null) {
+            if (fast == slow) {
+                return true;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return false;
+    }
+}
