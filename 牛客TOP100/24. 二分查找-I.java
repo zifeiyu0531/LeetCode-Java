f96819ed61a7de8f165ec629a176a9200b6fe5b9
zifeiyu0531 <32443516+zifeiyu0531@users.noreply.github.com>
2022-05-21 15:44:19 +0800

    二分查找-I

---
+++ 牛客TOP100/24. 二分查找-I.java
@@ -0,0 +1,26 @@
+package 牛客TOP100;
+
+class Solution {
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     *
+     * 
+     * @param nums   int整型一维数组
+     * @param target int整型
+     * @return int整型
+     */
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
