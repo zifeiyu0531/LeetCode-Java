a8237408ac913d8b1491060566d1b9e91fad11e5
zifeiyu0531 <32443516+zifeiyu0531@users.noreply.github.com>
2022-05-24 20:25:29 +0800

    最长公共前缀

---
+++ 牛客TOP100/32. 最长公共前缀.java
@@ -0,0 +1,23 @@
+package 牛客TOP100;
+
+class Solution {
+    /**
+     * 
+     * @param strs string字符串一维数组
+     * @return string字符串
+     */
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) {
+            return "";
+        }
+        for (int i = 0; i < strs[0].length(); i++) {
+            char ch = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++) {
+                if (i >= strs[j].length() || strs[j].charAt(i) != ch) {
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+        return strs[0];
+    }
+}
