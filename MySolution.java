23bf85a88160e877e8addc3d3453f8d57fc6126c
zifeiyu0531 <32443516+zifeiyu0531@users.noreply.github.com>
2022-05-24 20:25:33 +0800

    Create MySolution.java

---
+++ MySolution.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class MySolution {
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] >= nums[0]) {
+                if (nums[0] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[nums.length - 1] >= target && nums[mid] < target) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
